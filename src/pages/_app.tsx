d388637dda82ceef16b6354b873671816fb7f044
Batnaidan <batnaidan@gmail.com>
2025-11-21 20:08:32 +0800

    chore: removed unused trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import { type AppType } from "next/app";
 import "@fontsource/outfit";
 
-import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;

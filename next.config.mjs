7e02d03d482380acdde86aa4c502a0051bf94e00
Batnaidan <batnaidan@gmail.com>
2025-11-21 20:17:28 +0800

    feat: deploy script

--- next.config.mjs
+++ next.config.mjs
@@ -7,16 +7,9 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
 export default config;
